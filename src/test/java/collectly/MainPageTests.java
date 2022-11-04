81da14a7680dc170106ed4eea1d10eeb887830cb
Viktor_Rus <vaurusov@mts.ru>
2022-11-04 06:14:23 +0300

    first tests

--- src/test/java/collectly/MainPageTests.java
+++ src/test/java/collectly/MainPageTests.java
@@ -1,54 +1,33 @@
 package collectly;
 
 import io.qameta.allure.Owner;
-import org.junit.jupiter.api.BeforeEach;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import pages.MainPage;
 
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.sleep;
+
 
 public class MainPageTests extends TestBase {
 
-    @BeforeEach
-    @DisplayName("Открытие главной страницы")
-    public void visitMainPage() {
-        new MainPage().openMainPage();
-    }
+//    @BeforeEach
+//    @DisplayName("Открытие главной страницы")
+//    public void visitMainPage() {
+//        new MainPage().openMainPage();
+//    }
 
 
     @Test()
     @Owner("vaurusov")
     @DisplayName("Проверка отображения логотипа на главной странице")
     void mainLogoVisible() {
-        new MainPage().checkLogoVisible();
+
+       open("https://www.collectly.co/");
+       sleep(5000);
     }
 
-    @Test
-    @Owner("vaurusov")
-    @DisplayName("Проверка открытия формы 'Open an Account' ")
-    void openAccountForm() {
-        new MainPage().openAnAccount();
-    }
 
-    @Test
-    @Owner("vaurusov")
-    @DisplayName("Проверка переключателя языка в футере")
-    void changeLanguage() {
-        new MainPage().scrollToFooter()
-                      .choiceLanguageInFooter("Українська");
-    }
-
-    @Test
-    @Owner("vaurusov")
-    @DisplayName("Переход на страницу FAQ")
-    void visitPageFooter() {
-        new MainPage()
-                .hoverMenuNav("TRADING")
-                .clickByText("FAQ")
-                .checkSearchHeaderText("We've got answers to all of your questions");
-
-
-
-    }
 
 }
